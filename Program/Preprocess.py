05fa5f4c4c6ba0bcb4fa422bda9b90e56fd246c5
Marx.W <boyuan@vt.edu>
2019-09-23 18:14:08 -0400

    separated function

--- Program/Preprocess.py
+++ Program/Preprocess.py
@@ -1,5 +1,18 @@
 # this is for preprocess data
+import numpy as np
 
-#dfghhj
+f = open("../echo4/static-147-20190904_072253895067.txt", "r")
+a = f.readline()
 
-print('Hello World')
+with f as tsv:
+    line = [elem.strip().split('\t') for elem in tsv]
+dict = {}
+
+vals = np.asarray(line)
+mic0 = vals[::2]
+mic1 = vals[1::2]
+dict[0] = f.name;
+
+
+print(dict)
+
--- Program/data_break_down.py
+++ Program/data_break_down.py
@@ -1,15 +1,68 @@
+import os
 
 def split_data(filename):
     f= open(filename,"r")
-    m_1=[] #data from one microphone
-    m_2=[] #data from the other microphone
+    m_0=[] #data from one microphone
+    m_1=[] #data from the other microphone
 
     l = 0
     for i in f:
         if (l%2 == 0):
-            m_1.append(i)
+            m_0.append(i)
         else:
-            m_2.append(i)
+            m_1.append(i)
         l += 1
     f.close()
-    return [m_1,m_2]
+    return [m_0,m_1]
+
+def writeArrayToFile(folderpath, filename, m_0, m_1):
+    filename_0 = "0-" + filename;
+    filename_1 = "1-" + filename;
+
+    newfolderpath = folderpath + "/separated"
+    #os.mkdir(newfolderpath)
+    if not os.path.exists(newfolderpath+"/mic0"):
+        os.makedirs(newfolderpath+"/mic0")
+
+    if not os.path.exists(newfolderpath+"/mic1"):
+        os.makedirs(newfolderpath+"/mic1")
+
+    f = open(newfolderpath+"/mic0/"+filename_0, "w+")
+    for i in m_0:
+        f.write(str(i));
+    f.close();
+
+    f = open(newfolderpath+"/mic1/"+filename_1, "w+")
+    for i in m_1:
+        f.write(str(i));
+    f.close();
+
+    return 0;
+
+
+def separatefile(filePath):
+
+    filePath = "../" + filePath
+    fileList = os.listdir(filePath)
+    for filename in fileList:
+        if filename != "*.txt" :
+            fileList.remove(filename)
+
+    for filename in fileList:
+        print(filename)
+        incoming_file_path = filePath + "/" + filename;
+        if (filename != "separated"):
+            a = split_data(incoming_file_path)
+        writeArrayToFile(filePath, filename, a[0], a[1])
+
+
+
+    return 0;
+
+
+
+separatefile("echo4")
+separatefile("echo5_1")
+separatefile("echo5_2")
+print("writing finished");
+
