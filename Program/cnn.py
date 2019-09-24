1e52e12440bb45956673e33f46810fa066812814
Marx.W <boyuan@vt.edu>
2019-09-24 08:21:04 -0400

    temp

--- Program/cnn.py
+++ Program/cnn.py
@@ -2,7 +2,77 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import sys
 print(sys.version)
 import tensorflow as tf
-#tf.enable_eager_execution()
-tf.__version__
+import os
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+
+IMG_SIZE_X = 65
+IMG_SIZE_Y = 87
+
+def load_data(micName, IMG_SIZE_X, IMG_SIZE_Y):
+
+    this_path = os.getcwd()
+    #print(this_path)
+
+    filePath =  this_path + "/Fig/" + micName;
+    folderList = os.listdir(filePath)
+    #print(fileList)
+    
+
+    images_array = np.empty((1082+974,IMG_SIZE_X,IMG_SIZE_Y,3), dtype=float, order='C')
+    labels_array = np.empty((1082+974,1), dtype=float, order='C')
+    labels_array[0:1081] = 4
+    labels_array[1082:1082+974] = 5
+    #print(images_array)
+    #print(labels_array)
+    i = 0;
+
+    for folder in folderList:
+        folderPath = filePath + "/" + folder
+        picList = os.listdir(folderPath);
+        picList = [k for k in picList if '.jpg' in k];
+        #print(picList)
+        
+        for pic in picList:
+            temp_array = cv2.imread(folderPath+"/"+pic)
+            #print(folderPath+"/"+pic);
+            #print(i);
+            #print(pic);
+            #print(temp_array);
+            #if (temp_array != None):
+            if (check_array(temp_array)):
+              temp_array = cv2.resize(temp_array, (IMG_SIZE_Y, IMG_SIZE_X))
+              img_array = np.array(temp_array)
+              images_array[i] = img_array
+              i = i + 1;
+
+    return images_array[1:2056], labels_array[1:2056], images_array[0:2056:2], labels_array[0:2056:2];
 
 
+def check_array(x):
+    try:
+        x.shape
+        return True
+    except:
+        return False
+
+x_train, y_train, x_test, y_test = load_data("mic0", IMG_SIZE_X, IMG_SIZE_Y)
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(IMG_SIZE_X, IMG_SIZE_Y, 3)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=1)
+model.evaluate(x_test, y_test)
+
+
+print("program finished")
---
+++ echo4/bandpass_function.m
@@ -0,0 +1,54 @@
+function bandpass_function(FileName,DataPathWay,FigPathWay)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+%% Check Data
+cd_name=[DataPathWay];
+cd(cd_name) 
+data_mat = load(FileName);
+fs=400e3;% we are using 400e3;
+f = data_mat;
+t0= 0;  % Select the start time t0, time duration you want to do the bandpass
+t1= 1;  % Select the end time t1
+t=t0:1/fs:t1;
+inp=f;Alltime_start=[];Alltime_end=[];
+%% FIR Bandpass filter Parameters
+FilPara.Fstop1 = 19e3;      % First Stopband Frequency
+FilPara.Fpass1 = 21e3;      % First Passband Frequency
+FilPara.Fpass2 = 45e3;      % Second Passband Frequency
+FilPara.Fstop2 = 50e3;      % Second Stopband Frequency
+FilPara.Astop1 = 60;        % First Stopband Attenuation (dB)
+FilPara.Apass = 0.5;        % Passband Ripple (dB)
+FilPara.Astop2 = 60;        % Second Stopband Attenuation (dB)                            % fs: sampling frequency
+%% make the bandpass filter 
+h = fdesign.bandpass(FilPara.Fstop1, FilPara.Fpass1, FilPara.Fpass2, FilPara.Fstop2, FilPara.Astop1, FilPara.Apass, FilPara.Astop2, fs);
+Hd = design(h, 'equiripple');
+fil=Hd.Numerator;
+%% save the filter
+name=['Band_fil_' num2str(fs) '_' num2str(FilPara.Fpass1) '_' num2str(FilPara.Fpass2) '.wav'];
+save(name,'fil','fs','FilPara');
+%% Filter Process
+out=filtfilt(fil,1,inp);
+%save('out','out','fs');% Save the filtered data
+
+%% Plot
+%f = figure('visible', 'off');
+spectrogram(out(:,1),hanning(256),128,256,fs,'yaxis');
+ [~,ps] = spectrogram(out(:,1),[],[],[],fs,'yaxis');
+%  colorbar('off'); 
+ 
+% title('')
+% xlabel('')
+% ylabel('')
+% set(gca,'xticklabel',{[]})
+% set(gca,'yticklabel',{[]})
+
+ylim ([15 55])
+
+NewFileName=erase(FileName,".txt"); 
+FigName=strcat(NewFileName,'.jpg'); 
+saveas(gcf, fullfile(FigPathWay, FigName), 'jpeg');
+
+%close(f)
+
+end
+
